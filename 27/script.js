ba4605742be09e3ebdd94b8bd0f6f472c8dc5614
chromee <odk.mmmm@gmail.com>
2021-03-12 10:59:19 +0900

    27

---
+++ 27/script.js
@@ -0,0 +1,258 @@
+onload = function () {
+    var c = document.getElementById('canvas');
+    c.width = 500;
+    c.height = 300;
+
+    var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var v_shader = create_shader('vs');
+    var f_shader = create_shader('fs');
+
+    var prg = create_program(v_shader, f_shader);
+
+    var attLocation = new Array();
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    // テクスチャ座標
+    attLocation[2] = gl.getAttribLocation(prg, 'textureCoord');
+
+    var attStride = new Array();
+    attStride[0] = 3;
+    attStride[1] = 4;
+    attStride[2] = 2;
+
+    // 頂点の位置
+    var position = [
+        -1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0
+    ];
+
+    // 頂点色
+    var color = [
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0
+    ];
+
+    // テクスチャ座標
+    var textureCoord = [
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 1.0
+    ];
+
+    // 頂点インデックス
+    var index = [
+        0, 1, 2,
+        3, 2, 1
+    ];
+
+    var vPosition = create_vbo(position);
+    var vColor = create_vbo(color);
+    var vTextureCoord = create_vbo(textureCoord);
+    var VBOList = [vPosition, vColor, vTextureCoord];
+    var iIndex = create_ibo(index);
+
+    set_attribute(VBOList, attLocation, attStride);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iIndex);
+
+    var uniLocation = new Array();
+    uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
+    uniLocation[1] = gl.getUniformLocation(prg, 'texture0');
+    uniLocation[2] = gl.getUniformLocation(prg, 'texture1');
+
+    var m = new matIV();
+    var mMatrix = m.identity(m.create());
+    var vMatrix = m.identity(m.create());
+    var pMatrix = m.identity(m.create());
+    var tmpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+
+    m.lookAt([0.0, 2.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    // =======================
+
+    gl.activeTexture(gl.TEXTURE0);
+    var texture0 = null, texture1 = null;
+    create_texture('texture0.png', 0);
+    create_texture('texture1.png', 1);
+
+    // =======================
+
+    // カウンタの宣言
+    var count = 0;
+
+    // 恒常ループ
+    (function () {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        count++;
+        var rad = (count % 360) * Math.PI / 180;
+
+        // =======================
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture0);
+        gl.uniform1i(uniLocation[1], 0);
+
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, texture1);
+        gl.uniform1i(uniLocation[2], 1);
+
+        // =======================
+
+        m.identity(mMatrix);
+        m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+
+        gl.flush();
+
+        // ループのために再帰呼び出し
+        setTimeout(arguments.callee, 1000 / 30);
+    })();
+
+
+    function create_shader(id) {
+        var shader;
+
+        var scriptElement = document.getElementById(id);
+
+        if (!scriptElement) { return; }
+
+        switch (scriptElement.type) {
+            case 'x-shader/x-vertex':
+                shader = gl.createShader(gl.VERTEX_SHADER);
+                break;
+            case 'x-shader/x-fragment':
+                shader = gl.createShader(gl.FRAGMENT_SHADER);
+                break;
+            default:
+                return;
+        }
+
+        gl.shaderSource(shader, scriptElement.text);
+
+        gl.compileShader(shader);
+
+        if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            return shader;
+        }
+        else {
+            alert(gl.getShaderInfoLog(shader));
+        }
+    }
+
+    function create_program(vs, fs) {
+        var program = gl.createProgram();
+
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+
+        gl.linkProgram(program);
+
+        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            gl.useProgram(program);
+
+            return program;
+        }
+        else {
+            alert(gl.getProgramInfoLog(program));
+        }
+    }
+
+    function create_vbo(data) {
+        var vbo = gl.createBuffer();
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        return vbo;
+    }
+
+    function set_attribute(vbo, attL, attS) {
+        for (var i in vbo) {
+            // バッファをバインドする
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+
+            // attributeLocationを有効にする
+            gl.enableVertexAttribArray(attL[i]);
+
+            // attributeLocationを通知し登録する
+            gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+        }
+    }
+
+    function create_ibo(data) {
+        // バッファオブジェクトの生成
+        var ibo = gl.createBuffer();
+
+        // バッファをバインドする
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+
+        // バッファにデータをセット
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+
+        // バッファのバインドを無効化
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+        // 生成したIBOを返して終了
+        return ibo;
+    }
+
+    function create_texture(source) {
+        // イメージオブジェクトの生成
+        var img = new Image();
+
+        // データのオンロードをトリガーにする
+        img.onload = function () {
+            // テクスチャオブジェクトの生成
+            var tex = gl.createTexture();
+
+            // テクスチャをバインドする
+            gl.bindTexture(gl.TEXTURE_2D, tex);
+
+            // テクスチャへイメージを適用
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+
+            // ミップマップを生成
+            gl.generateMipmap(gl.TEXTURE_2D);
+
+            // テクスチャのバインドを無効化
+            gl.bindTexture(gl.TEXTURE_2D, null);
+
+            // 生成したテクスチャをグローバル変数に代入
+            switch (number) {
+                case 0:
+                    texture0 = tex;
+                    break;
+                case 1:
+                    texture1 = tex;
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        // イメージオブジェクトのソースを指定
+        img.src = source;
+    }
+};
